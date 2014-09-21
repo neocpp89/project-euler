5343b6a409c46015b567244d0a12c83291c93164
Sachith Dunatunga <neocpp@gmail.com>
2014-09-21 17:52:55 -0400

    Found a much faster way to do problem 69.

--- id69/id69.py
+++ id69/id69.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import math
 lim = 10 ** 6
+plim = 10 ** 3
 
 def isprime(n):
     if (n == 0 or n == 1):
@@ -14,7 +15,7 @@ def isprime(n):
         d += 1
     return True
 
-# primes = filter(isprime, range(0, 1+lim))
+primes = filter(isprime, range(0, 1+plim))
 
 def prime_factors(n):
     lim = int(math.floor(math.sqrt(n)))
@@ -37,5 +38,22 @@ def totient(n):
 
     return r
 
-t = map(lambda x: float(x)/totient(x), range(2, 1+lim))
-print (2+t.index(max(t)))
+# t = map(lambda x: float(x)/totient(x), range(2, 1+lim))
+# print (2+t.index(max(t)))
+
+def n_over_phi_n(n):
+    pf = list(set(prime_factors(n)))
+
+    r = 1
+    for p in pf:
+        r *= p / (p - 1.0)
+
+    return r
+
+n = 1
+i = 0
+while (n * primes[i] < lim):
+    n *= primes[i]
+    i += 1
+
+print n
